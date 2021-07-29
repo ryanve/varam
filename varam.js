70c64c5739300ab10b6d94e2f11a42a4ffdbf72f
Ryanvé <ryanve@gmail.com>
2021-07-29 00:00:13 -0400

    staging via meta for the win (#8)
    * 79 lines elmo flow help meta reset staff stage state via seed seeded
    
    * 77 later darling stage staff
    
    * hyperlink staging #8

--- varam.js
+++ varam.js
@@ -1,15 +1,18 @@
-!function(web) {
+//ryanve.dev/varam
+!function(web, dom) {
+  var empty = ""
   var dash = "--"
   var word = /\S+/g
   var what = "data-varam"
   var where = "[data-varam]"
-  var all = "querySelectorAll"
+  var first = dom.querySelector || no
   var par = web.URLSearchParams
+  var api = par ? varam : no
 
   function varam(search) {
     var did = false
     var url = new par(search)
-    var stack = document[all](where)
+    var stack = dom.querySelectorAll(where)
     each(stack, function(scope) {
       var keys = scope.getAttribute(what)
       keys = keys && keys.match(word)
@@ -20,11 +23,25 @@
         did = fresh(style, relay, value) || did
       })
     })
+    api.state = url.toString()
     return did
   }
 
   function no() {
     return false
+  }
+
+  function flow() {
+    return api(location.search)
+  }
+
+  function reset() {
+    return api(empty)
+  }
+
+  function help() {
+    console.dir(api)
+    return api.state
   }
 
   function fresh(style, relay, value) {
@@ -40,16 +57,21 @@
     while(i--) f(stack[i])
   }
 
-  var api = par ? varam : no
-  var seed = web.varam
+  var like = "meta[name=varam][content]"
+  var elmo = first.call(dom, like)
+  var seed = elmo && elmo.content
   var seeded = typeof seed == "string"
   seed = seeded && seed
-  api.seed = seed
-  api.seeded = seeded
+  api.flow = par ? flow : no
   api.fresh = fresh
+  api.help = help
+  api.reset = par ? reset : no
+  api.state = empty
+  par && api(seeded ? seed : location.search)
+  api.meta = seeded ? api.state : false
+  api.via = seeded ? "meta" : "flow"
   web.varam = api
-  api(seeded ? seed : location.search)
   var common = typeof module != "undefined"
   if (common && module.exports)
     module.exports = api
-}(window)
+}(window, document)
